ca1404418443ef3112a9f54cb77f776dd923fdf6
xiaohubai <xiaohubai@outlook.com>
2022-10-25 18:08:57 +0800

    完善启动脚本

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,3 +3,10 @@ docker start mysql
 docker start redis
 docker start elasticsearch
 docker start jaeger
+docker start grafana
+docker start kibana
+docker start kafka
+docker start consul
+docker start nginx
+docker start prometheus
+docker start zookeeper
